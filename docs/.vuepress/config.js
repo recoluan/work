34c05e1755338babf66a5e5659aca2fb335a7fd8
reco_luan <luanhewei@gmail.com>
2019-05-09 13:30:06 +0800

    测试

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,6 @@
 module.exports = {
-  // base: '/work/',
-  title: "reco-work",
-  description: '记下来，永远不要相信自己的脑子！',
+  title: "午后南杂",
+  description: 'Enjoy when you can, and endure when you must.',
   dest: 'public',
   head: [
     ['link', { rel: 'icon', href: '/favicon.ico' }],
@@ -10,26 +9,21 @@ module.exports = {
   theme: 'reco',
   themeConfig: {
     nav: [
-      { text: '首页', link: '/', icon: 'reco-home' },
-      { text: 'TimeLine', link: '/timeLine/', icon: 'reco-date' },
-      { text: '午后南杂', link: 'https://recoluan.gitlab.io', icon: 'reco-messag' },
+      { text: 'Home', link: '/', icon: 'reco-home' },
+      { text: 'TimeLine', link: '/timeline/', icon: 'reco-date' },
+      { text: 'Three-Month Study', link: '/views/threeMonthStudy/', icon: 'reco-three' },
+      { text: 'Contact', 
+        icon: 'reco-message',
+        items: [
+          { text: 'NPM', link: 'https://www.npmjs.com/~reco_luan', icon: 'reco-npm' },
+          { text: 'GitHub', link: 'https://github.com/recoluan', icon: 'reco-github' },
+          { text: '简书', link: 'https://www.jianshu.com/u/cd674a19515e', icon: 'reco-jianshu' },
+          { text: 'CSDN', link: 'https://blog.csdn.net/recoluan', icon: 'reco-csdn' },
+          { text: '博客圆', link: 'https://www.cnblogs.com/luanhewei/', icon: 'reco-bokeyuan' },
+          { text: 'WeChat', link: 'https://mp.weixin.qq.com/s/mXFqeUTegdvPliXknAAG_A', icon: 'reco-wechat' },
+        ]
+      }
     ],
-    logo: '/head.png',
-    //search config 
-    search: true,
-    searchMaxSuggestions: 10,
-    // auto create sidebar
-    sidebar: 'auto',
-    // last Updated Date
-    lastUpdated: 'Last Updated', // string | boolean
-    // author
-    author: 'reco_luan',
-    // 密钥
-    keyPage: {
-      keys: ['930105'],
-      color: '#424242',
-      lineColor: '#424242'
-    },
     // 博客设置
     blogConfig: {
       category: {
@@ -41,6 +35,34 @@ module.exports = {
         text: 'Tag' // 默认 “标签”
       }
     },
+    logo: '/head.png',
+    // 搜索设置
+    search: true,
+    searchMaxSuggestions: 10,
+    // 自动形成侧边导航
+    sidebar: 'auto',
+    // 最后更新时间
+    lastUpdated: 'Last Updated', // string | boolean
+    // 作者
+    author: 'reco_luan',
+    // 密钥
+    // keyPage: {
+    //   keys: ['930105'],
+    //   color: '#42b983',
+    //   lineColor: '#42b983'
+    // },
+    // valine 设置
+    valineConfig: {
+      appId: 'Q6hMeY2PSaM9FMkXetzoJoU5-gzGzoHsz',
+      appKey: 'iLQlev5jo2Cm5pLcI0z3qhtr',
+      placeholder: '填写邮箱可以收到回复提醒哦！',
+      notify: true,
+      recordIP: true
+    },
+    // 备案号
+    record: '京ICP备17067634号-1',
+    // 项目开始时间
+    startYear: '2017'
   },
   markdown: {
     lineNumbers: true
