3eb1e77c04bb06da6076450f99c2c209a472efd8
reco_luan <recoluan@outlook.com>
2020-03-29 20:11:15 +0800

    feat: add doc

--- docs/.vuepress/config/sidebar/dataStructuresAndAlgorithms.js
+++ docs/.vuepress/config/sidebar/dataStructuresAndAlgorithms.js
@@ -8,6 +8,9 @@ module.exports = {
         'algorithms/2020/032701',
         'algorithms/2020/032702',
         'algorithms/2020/032703',
+        'algorithms/2020/032704',
+        'algorithms/2020/032705',
+        'algorithms/2020/032706'
       ]
     },
     {
